03b97f11750fa00fe4aa287be8694f69adc85102
Unknown <cristianignat97@gmail.com>
2019-03-03 18:48:14 +0200

    decrypted on m side

--- Client.py
+++ Client.py
@@ -7,6 +7,8 @@ from Crypto.Random import get_random_bytes
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Utils import saveKey, loadKey
+from Crypto.Util.Padding import pad,unpad
+from base64 import b64decode
 import socket
 import json
 import pickle
@@ -19,14 +21,7 @@ import pickle
 # print(loadKey('cSK'))
 
 
-merchant_publicKey = loadKey('mPK')
 
-aes_bytekey = get_random_bytes(32)
-
-cipher_rsa = PKCS1_OAEP.new(RSA.import_key(merchant_publicKey))
-merchant_encrypted_key = cipher_rsa.encrypt(aes_bytekey)
-
-aes_key = AES.new(aes_bytekey,AES.MODE_CBC)
 
 # print(merchant_encrypted_key)
 
@@ -39,8 +34,27 @@ PORT = 6001
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.connect((TCP_IP,PORT))
-# print("Len",len(merchant_encrypted_key))
+"""
+    Send aes key encrypted with merchent publickey rsa
+"""
+#######################################################################
+merchant_publicKey = loadKey('mPK')
+
+aes_bytekey = get_random_bytes(32)
+
+cipher_rsa = PKCS1_OAEP.new(RSA.import_key(merchant_publicKey))
+merchant_encrypted_key = cipher_rsa.encrypt(aes_bytekey)
+
+aes_key = AES.new(aes_bytekey,AES.MODE_CBC)
 server.send(merchant_encrypted_key)
+
+#####################################################################
+
+"""
+    Recieve the SID and SSID and verify them
+    
+"""
+#######################################################################################
 signature = server.recv(3024)
 print("reciving..", signature)
 # print(len(signature))
@@ -48,7 +62,57 @@ signature = pickle.loads(signature)
 print(signature)
 
 key = RSA.import_key(loadKey('mPK'))
-print("Verify",pkcs1_15.new(key).verify(SHA256.new(signature['SID']),(signature['SSID'])))
+
+verification = None
+try:
+    pkcs1_15.new(key).verify(SHA256.new(signature['SID']), (signature['SSID']))
+    verification = True
+except (ValueError, TypeError):
+    verification= False
+
+print(verification)
+
+#########################################################################################
+"""
+    
+    Criptam PM si PO cu aes si le trimitem merchantului
+
+"""
+########################################################################################
+
+PI = {'CardNumber':'XXXX-XXXX-XXXX-XXXX','CardExp':'07/20','CCode':764,'cPK':loadKey('cPK')}
+PI_bytes = pickle.dumps(PI)
+print("PI_Bytes",PI_bytes)
+
+PM = (PI_bytes,pkcs1_15.new(RSA.import_key(loadKey('cSK'))).sign(SHA256.new(PI_bytes)))
+
+PM_bytes = pickle.dumps(PM)
+
+PO_withoutsig = {'OrderDescription':'Some description','SID':signature['SID'],'Amount':9000}
+PO_withoutsig_bytes = pickle.dumps(PO_withoutsig)
+
+PO = {'OrderDescription':'Some description','SID':signature['SID'],'Amount':9000,'SigC':pkcs1_15.new(RSA.import_key(loadKey('cSK'))).sign(SHA256.new(PO_withoutsig_bytes))}
+PO_bytes = pickle.dumps(PO)
+print("PICKLE_LOADS\n",pickle.loads(PO_bytes))
+
+aes_key = AES.new(aes_bytekey,AES.MODE_CBC)
+
+
+PO_encrypted = aes_key.encrypt(pad(PO_bytes,AES.block_size))
+iv = aes_key.iv
+print("iv1,",iv)
+server.send(iv)
+server.send(PO_encrypted)
+
+aes_key = AES.new(aes_bytekey,AES.MODE_CBC)
+
+
+PM_encrypted = aes_key.encrypt(pad(PM_bytes,AES.block_size))
+iv = aes_key.iv
+print("iv2,",iv)
+server.send(iv)
+server.send(PM_encrypted)
 
 
 
+########################################################################################
--- Merchant.py
+++ Merchant.py
@@ -1,21 +1,27 @@
 import Crypto
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Hash import SHA256
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from Crypto.Signature import pkcs1_15
+from Crypto.Util.Padding import pad,unpad
 import json
 import pickle
 
 from Utils import saveKey, loadKey
 import socket
 
-
-def signSessionId(aes_encrypted_key):
+def decryptAESKey(aes_encrypted_key):
     merchant_secretKey = loadKey('mSK')
     cipher_rsa = PKCS1_OAEP.new(RSA.import_key(merchant_secretKey))
     aes_decrypted_key = cipher_rsa.decrypt(aes_encrypted_key)
+
+    return aes_decrypted_key
+
+def signSessionId(aes_encrypted_key):
+    aes_decrypted_key = decryptAESKey(aes_encrypted_key)
     print(aes_decrypted_key)
 
     SID = get_random_bytes(8)
@@ -32,6 +38,16 @@ def signSessionId(aes_encrypted_key):
 
     # Return session id and signed session id with merchant private key
     return (SID, SSID)
+
+def decryptAES_item(item,aes_encrypted_key,iv):
+    print("IV", iv)
+    aes_decrypted_key = decryptAESKey(aes_encrypted_key)
+    cipher = AES.new(aes_decrypted_key,AES.MODE_CBC,iv)
+
+    item_decrypted = unpad(cipher.decrypt(item),AES.block_size)
+
+    print("PO_DECRIPT\n",item_decrypted)
+    print("PICKE", pickle.loads(item_decrypted))
 
 
 # random_generator = Random.new().read
@@ -56,11 +72,19 @@ data = None
 data = conn.recv(3024)
 if len(data) > 0:
     # print("Len", data)
-    SID, SSID = signSessionId(data)
+    aes_encrypted_key = data
+    SID, SSID = signSessionId(aes_encrypted_key)
     msg = pickle.dumps({'SID': SID, 'SSID': SSID})
     print("sending..", msg)
     conn.send(msg)
+    iv= conn.recv(2048)
+    PO_encrypted = conn.recv(2048)
+    decryptAES_item(PO_encrypted, aes_encrypted_key, iv)
 
+    iv = conn.recv(1024)
+    PM_encrypted = conn.recv(2048)
+
+    decryptAES_item(PM_encrypted,aes_encrypted_key,iv)
     # conn.send('aaa')
 
 conn.close()
